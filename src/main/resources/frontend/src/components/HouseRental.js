2910ea4ce1064516700a83cd5ad2c52bbbe107f9
Hampus Nilsson <hampus10699@gmail.com>
2020-03-26 23:36:34 +0100

    added a new npm package.
    Added a new npm package for calender in add House for rental component. The Npm package is react-calendar.

--- src/main/resources/frontend/src/components/HouseRental.js
+++ src/main/resources/frontend/src/components/HouseRental.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Col,
   Button,
@@ -8,18 +8,28 @@ import {
   Input,
   FormText
 } from "reactstrap";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+
 
 const HouseRental = () => {
+
+  const [date, setDate] = useState('')
+
+  onchange = date => setDate({ date })
+
   return (
     <Form>
       <h1 className="text-center">Lägg till bostad</h1>
       <FormGroup row>
-        <Label for="select-image" sm={2}>Ladda upp bilder</Label>
-        <Col sm={10} >
-          <Input type="file" name="image" id="select-image"/>
+        <Label for="select-image" sm={2}>
+          Ladda upp bilder
+        </Label>
+        <Col sm={10}>
+          <Input type="file" name="image" id="select-image" />
         </Col>
       </FormGroup>
-      <FormGroup row>
+      <FormGroup row >
         <Label for="title-input" sm={2}>
           Rubrik
         </Label>
@@ -35,7 +45,7 @@ const HouseRental = () => {
           <Input type="textarea" name="text" id="description-input" />
         </Col>
       </FormGroup>
-      
+
       <FormGroup row>
         <Label for="checkbox2" sm={2}>
           Bekvämligheter
@@ -63,12 +73,14 @@ const HouseRental = () => {
           </FormGroup>
           <FormGroup check>
             <Label check>
-              <Input type="checkbox" id="free-parking-checkbox" /> Gratis parkering
+              <Input type="checkbox" id="free-parking-checkbox" /> Gratis
+              parkering
             </Label>
           </FormGroup>
           <FormGroup check>
             <Label check>
-              <Input type="checkbox" id="washing-machine-checkbox" /> Tvättmaskin
+              <Input type="checkbox" id="washing-machine-checkbox" />{" "}
+              Tvättmaskin
             </Label>
           </FormGroup>
           <FormGroup check>
@@ -93,9 +105,15 @@ const HouseRental = () => {
           </FormGroup>
         </Col>
       </FormGroup>
-      <FormGroup check row>
+      <FormGroup className="d-flex justify-content-center">
+          <Calendar
+            onChange={onchange}
+            value={date}>
+          </Calendar>
+      </FormGroup>
+      <FormGroup check row className="text-center">
         <Col sm={{ size: 10, offset: 2 }}>
-          <Button>Lägg till för uthyrning</Button>
+          <Button className="btn btn-success" >Lägg till för uthyrning</Button>
         </Col>
       </FormGroup>
     </Form>
@@ -103,3 +121,5 @@ const HouseRental = () => {
 };
 
 export default HouseRental;
+
+// calendar npm = https://www.npmjs.com/package/react-calendar
