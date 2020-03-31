c804bdaab749789fdd9e2477979f643f0f31d99d
ironboy <thomas@nodehill.com>
2020-03-31 10:46:01 +0200

    indent JS using 2 spaces!

--- src/main/resources/frontend/src/components/ResidenceList.js
+++ src/main/resources/frontend/src/components/ResidenceList.js
@@ -1,27 +1,27 @@
 import React, { useContext } from 'react'
 import { ResidenceContext } from '../contexts/ResidenceContextProvider'
-import {SearchResidence} from '../components/SearchResidence'
+import { SearchResidence } from '../components/SearchResidence'
 
 
 
 export default function ResidenceList() {
-  const {residences} = useContext(ResidenceContext)
+  const { residences } = useContext(ResidenceContext)
 
-    const list = () => {
-        return residences.map((residence, i) => {
-            return (
-                <div>
-                <p key={residence.id + i}>Recidence id: {residence.id}</p>
-            <p key={residence.max_guests + i}>Max Guests: {residence.max_guests}</p>
-                </div>
+  const list = () => {
+    return residences.map((residence, i) => {
+      return (
+        <div>
+          <p key={residence.id + i}>Recidence id: {residence.id}</p>
+          <p key={residence.max_guests + i}>Max Guests: {residence.max_guests}</p>
+        </div>
 
-            )
-        })
-    }
-  
-    return (
-      <>
-        {list()}
-      </>
-    )
+      )
+    })
   }
+
+  return (
+    <>
+      {list()}
+    </>
+  )
+}
