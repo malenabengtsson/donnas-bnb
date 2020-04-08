a25ec12f7a4bd620e974fecd69165fa4174d3be6
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-04-08 12:06:14 +0200

    removed test-testing

--- src/main/resources/frontend/src/pages/Home.js
+++ src/main/resources/frontend/src/pages/Home.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react'
 import { Button, Form, FormGroup, Label, Input, FormText, Col, Row, Container } from 'reactstrap';
 import SearchResidence from '../components/SearchResidence'
-import CustomButton from '../components/CustomButton'
+
 
 const Home = () => {
 
   return (
     <div>
-      <CustomButton label="click me please"/>
       <SearchResidence />
     </div>
   )
