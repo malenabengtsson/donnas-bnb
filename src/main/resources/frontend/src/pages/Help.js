70db87e3450e496b165671f5eb6909fa12c4c53d
André <andreingvarsson@hotmail.com>
2020-04-13 10:14:47 +0200

    Added links to register and login

--- src/main/resources/frontend/src/pages/Help.js
+++ src/main/resources/frontend/src/pages/Help.js
@@ -1,8 +1,20 @@
-import React from 'react'
-import { UncontrolledCollapse, Button, CardBody, Card, Row } from 'reactstrap'
+import React, { useState } from 'react'
+import { UncontrolledCollapse, Button, CardBody, Card, Row,} from 'reactstrap'
+import { Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Help = () =>{
+
+
+    const [gotoLogin, setGotoLogin] = useState(false)
+    const [gotoRegister, setGotoRegister] = useState(false)
     
+    const initSearch = (e) => {
+        e.preventDefault()
+
+        setGotoLogin(true)
+        
+    }
 
     return(
         <div className="backGround" >
@@ -10,6 +22,7 @@ const Help = () =>{
                 Frågor och svar
             </h2>
             <div>
+           
                 <Button color="success" id="registerToggler" style={{ marginBottom: '1rem', marginLeft: '10px'}}>Registering?</Button>
                     <UncontrolledCollapse toggler="#registerToggler">
                         <Card style={{ width: '100%', height: 'auto'}}>
@@ -17,6 +30,7 @@ const Help = () =>{
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni, voluptas debitis
                             similique porro a molestias consequuntur earum odio officiis natus, amet hic, iste sed
                             dignissimos esse fuga! Minus, alias.
+                            <p><Link to="/perform-register">Klicka här</Link></p>
                             </CardBody>
                         </Card>
                     </UncontrolledCollapse>          
@@ -29,6 +43,7 @@ const Help = () =>{
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni, voluptas debitis
                             similique porro a molestias consequuntur earum odio officiis natus, amet hic, iste sed
                             dignissimos esse fuga! Minus, alias.
+                            <p><Link to="/perform-login">Klicka här</Link></p>
                             </CardBody>
                         </Card>
                     </UncontrolledCollapse>          
